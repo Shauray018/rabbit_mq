617a965f662c234a7d66b8e6bf2c85cdf80e7a44
Shauray018 <shauraydhingra03@gmail.com>
2026-03-28 19:35:00 +0530

    first commit

---
+++ consumer.js
@@ -0,0 +1,23 @@
+const amqp = require("amqplib");  
+
+
+async function recieveMail() {
+    try { 
+        const connection = await amqp.connect("amqp://localhost"); 
+        const channel = await connection.createChannel(); 
+
+        await channel.assertQueue("mail_queue", {durable: false}); 
+
+        channel.consume("mail_queue", (message) => { 
+            if (message !== null) { 
+                console.log("Recv message", JSON.parse(message.content)); 
+                channel.ack(message); 
+            }
+        })
+    } catch(error) { 
+        console.log(error); 
+    }
+    
+}
+
+recieveMail(); 
---
+++ producer.js
@@ -0,0 +1,33 @@
+const amqp = require("amqplib"); 
+
+async function sendMail() {
+    try { 
+        const connection = await amqp.connect("amqp://localhost"); 
+        const channel = await connection.createChannel(); 
+        const exchange = "mail_exchange"; 
+        const routingKey = "send_mail"; 
+
+        const message = { 
+            to: "arun@gmail.com",
+            from: "oji@gmail.com", 
+            subject: "thanks", 
+            body: "hello chacha"
+        }
+
+        await channel.assertExchange(exchange, "direct", {durable: false}); 
+        await channel.assertQueue("mail_queue", {durable: false});
+
+        await channel.bindQueue("mail_queue", exchange, routingKey); 
+
+        channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(message))); 
+        console.log("Mail data was sent", message); 
+
+        setTimeout(() => { 
+            connection.close(); 
+        }, 500); 
+    } catch (error) { 
+        console.log(error);
+    }
+}
+
+sendMail(); 
